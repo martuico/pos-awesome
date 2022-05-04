91a47f9d9a89ec5b96214b1fcccf386098f1a7a4
Mar Tuico <mar.tuico@gmail.com>
2022-05-05 00:57:45 +0800

    fixed conflict

--- posawesome/public/dist/js/posapp.min.bundle.BWSOOYUK.js
+++ posawesome/public/dist/js/posapp.min.bundle.BWSOOYUK.js
@@ -23488,148 +23488,41 @@ export default {
 
   // ../posawesome/posawesome/public/js/posapp/components/pos/Bookings.vue
   var __vue_script__18 = {
+    components: {
+      Customer: Customer_default
+    },
     data() {
       return {
-        itemsPerPageArray: [4, 8, 12],
-        search: "",
-        filter: {},
-        sortDesc: false,
-        page: 1,
-        itemsPerPage: 4,
-        sortBy: "name",
-        keys: [
-          "Name",
-          "Calories",
-          "Fat",
-          "Carbs",
-          "Protein",
-          "Sodium",
-          "Calcium",
-          "Iron"
-        ],
-        items: [
-          {
-            name: "Frozen Yogurt",
-            calories: 159,
-            fat: 6,
-            carbs: 24,
-            protein: 4,
-            sodium: 87,
-            calcium: "14%",
-            iron: "1%"
-          },
-          {
-            name: "Ice cream sandwich",
-            calories: 237,
-            fat: 9,
-            carbs: 37,
-            protein: 4.3,
-            sodium: 129,
-            calcium: "8%",
-            iron: "1%"
-          },
-          {
-            name: "Eclair",
-            calories: 262,
-            fat: 16,
-            carbs: 23,
-            protein: 6,
-            sodium: 337,
-            calcium: "6%",
-            iron: "7%"
-          },
-          {
-            name: "Cupcake",
-            calories: 305,
-            fat: 3.7,
-            carbs: 67,
-            protein: 4.3,
-            sodium: 413,
-            calcium: "3%",
-            iron: "8%"
-          },
-          {
-            name: "Gingerbread",
-            calories: 356,
-            fat: 16,
-            carbs: 49,
-            protein: 3.9,
-            sodium: 327,
-            calcium: "7%",
-            iron: "16%"
-          },
-          {
-            name: "Jelly bean",
-            calories: 375,
-            fat: 0,
-            carbs: 94,
-            protein: 0,
-            sodium: 50,
-            calcium: "0%",
-            iron: "0%"
-          },
-          {
-            name: "Lollipop",
-            calories: 392,
-            fat: 0.2,
-            carbs: 98,
-            protein: 0,
-            sodium: 38,
-            calcium: "0%",
-            iron: "2%"
-          },
-          {
-            name: "Honeycomb",
-            calories: 408,
-            fat: 3.2,
-            carbs: 87,
-            protein: 6.5,
-            sodium: 562,
-            calcium: "0%",
-            iron: "45%"
-          },
-          {
-            name: "Donut",
-            calories: 452,
-            fat: 25,
-            carbs: 51,
-            protein: 4.9,
-            sodium: 326,
-            calcium: "2%",
-            iron: "22%"
-          },
-          {
-            name: "KitKat",
-            calories: 518,
-            fat: 26,
-            carbs: 65,
-            protein: 7,
-            sodium: 54,
-            calcium: "12%",
-            iron: "6%"
-          }
-        ]
+        pos_profile: "",
+        pos_opening_shift: "",
+        customer: ""
       };
     },
-    computed: {
-      numberOfPages() {
-        return Math.ceil(this.items.length / this.itemsPerPage);
-      },
-      filteredKeys() {
-        return this.keys.filter((key) => key !== "Name");
-      }
+    computed: {},
+    created: function() {
+      this.$nextTick(function() {
+        this.check_opening_entry();
+        evntBus.$on("update_customer", (customer) => {
+          this.customer = customer;
+        });
+      });
     },
     methods: {
-      nextPage() {
-        if (this.page + 1 <= this.numberOfPages)
-          this.page += 1;
-      },
-      formerPage() {
-        if (this.page - 1 >= 1)
-          this.page -= 1;
-      },
-      updateItemsPerPage(number) {
-        this.itemsPerPage = number;
+      check_opening_entry() {
+        return frappe.call("posawesome.posawesome.api.posapp.check_opening_shift", {
+          user: frappe.session.user
+        }).then((r) => {
+          if (r.message) {
+            this.pos_profile = r.message.pos_profile;
+            this.pos_opening_shift = r.message.pos_opening_shift;
+            this.get_offers(this.pos_profile.name);
+            evntBus.$emit("register_pos_profile", r.message);
+            evntBus.$emit("set_company", r.message.company);
+            console.info("LoadPosProfile");
+          } else {
+            this.create_opening_voucher();
+          }
+        });
       }
     }
   };
@@ -23648,20 +23541,19 @@ export default {
               staticClass: "selection mx-auto grey lighten-5",
               staticStyle: { "max-height": "75vh", height: "75vh" }
             }, [
-              _c("v-row", { staticClass: "items px-2 py-1" }, [
-                _c("v-text-field", {
-                  attrs: {
-                    dense: "",
-                    clearable: "",
-                    autofocus: "",
-                    outlined: "",
-                    color: "indigo",
-                    label: _vm.frappe._("Search Customer"),
-                    hint: "Seach by name",
-                    "background-color": "white",
-                    "hide-details": ""
-                  }
-                })
+              _c("v-row", {
+                staticClass: "items px-2 py-1",
+                attrs: { align: "center" }
+              }, [
+                _vm.pos_profile.posa_allow_sales_order ? _c("v-col", {
+                  staticClass: "pb-0 mb-2 pr-0",
+                  attrs: { cols: "9" }
+                }, [_c("Customer")], 1) : _vm._e(),
+                _vm._v(" "),
+                !_vm.pos_profile.posa_allow_sales_order ? _c("v-col", {
+                  staticClass: "pb-0 mb-2",
+                  attrs: { cols: "12" }
+                }, [_c("Customer")], 1) : _vm._e()
               ], 1)
             ], 1)
           ], 1)
@@ -23692,330 +23584,76 @@ export default {
             <v-card
               class="selection mx-auto grey lighten-5"
               style="max-height: 75vh; height: 75vh">
-              <v-row class="items px-2 py-1">
-                <v-text-field
-                  dense
-                  clearable
-                  autofocus
-                  outlined
-                  color="indigo"
-                  :label="frappe._('Search Customer')"
-                  hint="Seach by name"
-                  background-color="white"
-                  hide-details
-                ></v-text-field>
+                <v-row align="center" class="items px-2 py-1">
+                  <v-col
+                    v-if="pos_profile.posa_allow_sales_order"
+                    cols="9"
+                    class="pb-0 mb-2 pr-0"
+                  >
+                    <Customer></Customer>
+                  </v-col>
+                  <v-col
+                    v-if="!pos_profile.posa_allow_sales_order"
+                    cols="12"
+                    class="pb-0 mb-2"
+                  >
+                    <Customer></Customer>
+                  </v-col>
               </v-row>
             </v-card>
           </div>
         </v-col>
       </v-row>
-    <!-- <v-data-iterator
-      :items="items"
-      :items-per-page.sync="itemsPerPage"
-      :page.sync="page"
-      :search="search"
-      :sort-by="sortBy.toLowerCase()"
-      :sort-desc="sortDesc"
-      hide-default-footer
-    >
-      <template v-slot:header>
-        <v-toolbar
-          dark
-          color="blue darken-3"
-          class="mb-1"
-        >
-          <v-text-field
-            v-model="search"
-            clearable
-            flat
-            solo-inverted
-            hide-details
-            prepend-inner-icon="mdi-magnify"
-            label="Search"
-          ></v-text-field>
-          <template v-if="$vuetify.breakpoint.mdAndUp">
-            <v-spacer></v-spacer>
-            <v-select v-model="sortBy"
-              flat
-              solo-inverted
-              hide-details
-              :items="keys"
-              prepend-inner-icon="mdi-magnify"
-              label="Sort by"
-            ></v-select>
-            <v-spacer></v-spacer>
-            <v-btn-toggle
-              v-model="sortDesc"
-              mandatory
-            >
-              <v-btn
-                large
-                depressed
-                color="blue"
-                :value="false"
-              >
-                <v-icon>mdi-arrow-up</v-icon>
-              </v-btn>
-              <v-btn
-                large
-                depressed
-                color="blue"
-                :value="true"
-              >
-                <v-icon>mdi-arrow-down</v-icon>
-              </v-btn>
-            </v-btn-toggle>
-          </template>
-        </v-toolbar>
-      </template>
-
-      <template v-slot:default="props">
-        <v-row>
-          <v-col
-            v-for="item in props.items"
-            :key="item.name"
-            cols="12"
-            sm="6"
-            md="4"
-            lg="3"
-          >
-            <v-card>
-              <v-card-title class="subheading font-weight-bold">
-                {{ item.name }}
-              </v-card-title>
-
-              <v-divider></v-divider>
-
-              <v-list dense>
-                <v-list-item
-                  v-for="(key, index) in filteredKeys"
-                  :key="index"
-                >
-                  <v-list-item-content :class="{ 'blue--text': sortBy === key }">
-                    {{ key }}:
-                  </v-list-item-content>
-                  <v-list-item-content
-                    class="align-end"
-                    :class="{ 'blue--text': sortBy === key }"
-                  >
-                    {{ item[key.toLowerCase()] }}
-                  </v-list-item-content>
-                </v-list-item>
-              </v-list>
-            </v-card>
-          </v-col>
-        </v-row>
-      </template>
-
-      <template v-slot:footer>
-        <v-row
-          class="mt-2"
-          align="center"
-          justify="center"
-        >
-          <span class="grey--text">Items per page</span>
-          <v-menu offset-y>
-            <template v-slot:activator="{ on, attrs }">
-              <v-btn
-                dark
-                text
-                color="primary"
-                class="ml-2"
-                v-bind="attrs"
-                v-on="on"
-              >
-                {{ itemsPerPage }}
-                <v-icon>mdi-chevron-down</v-icon>
-              </v-btn>
-            </template>
-            <v-list>
-              <v-list-item
-                v-for="(number, index) in itemsPerPageArray"
-                :key="index"
-                @click="updateItemsPerPage(number)"
-              >
-                <v-list-item-title>{{ number }}</v-list-item-title>
-              </v-list-item>
-            </v-list>
-          </v-menu>
-
-          <v-spacer></v-spacer>
-
-          <span
-            class="mr-4
-            grey--text"
-          >
-            Page {{ page }} of {{ numberOfPages }}
-          </span>
-          <v-btn
-            fab
-            dark
-            color="blue darken-3"
-            class="mr-1"
-            @click="formerPage"
-          >
-            <v-icon>mdi-chevron-left</v-icon>
-          </v-btn>
-          <v-btn
-            fab
-            dark
-            color="blue darken-3"
-            class="ml-1"
-            @click="nextPage"
-          >
-            <v-icon>mdi-chevron-right</v-icon>
-          </v-btn>
-        </v-row>
-      </template>
-    </v-data-iterator> -->
   </v-container>
 </template>
 
 <script>
-  export default {
-    data () {
-      return {
-        itemsPerPageArray: [4, 8, 12],
-        search: '',
-        filter: {},
-        sortDesc: false,
-        page: 1,
-        itemsPerPage: 4,
-        sortBy: 'name',
-        keys: [
-          'Name',
-          'Calories',
-          'Fat',
-          'Carbs',
-          'Protein',
-          'Sodium',
-          'Calcium',
-          'Iron',
-        ],
-        items: [
-          {
-            name: 'Frozen Yogurt',
-            calories: 159,
-            fat: 6.0,
-            carbs: 24,
-            protein: 4.0,
-            sodium: 87,
-            calcium: '14%',
-            iron: '1%',
-          },
-          {
-            name: 'Ice cream sandwich',
-            calories: 237,
-            fat: 9.0,
-            carbs: 37,
-            protein: 4.3,
-            sodium: 129,
-            calcium: '8%',
-            iron: '1%',
-          },
-          {
-            name: 'Eclair',
-            calories: 262,
-            fat: 16.0,
-            carbs: 23,
-            protein: 6.0,
-            sodium: 337,
-            calcium: '6%',
-            iron: '7%',
-          },
-          {
-            name: 'Cupcake',
-            calories: 305,
-            fat: 3.7,
-            carbs: 67,
-            protein: 4.3,
-            sodium: 413,
-            calcium: '3%',
-            iron: '8%',
-          },
-          {
-            name: 'Gingerbread',
-            calories: 356,
-            fat: 16.0,
-            carbs: 49,
-            protein: 3.9,
-            sodium: 327,
-            calcium: '7%',
-            iron: '16%',
-          },
-          {
-            name: 'Jelly bean',
-            calories: 375,
-            fat: 0.0,
-            carbs: 94,
-            protein: 0.0,
-            sodium: 50,
-            calcium: '0%',
-            iron: '0%',
-          },
-          {
-            name: 'Lollipop',
-            calories: 392,
-            fat: 0.2,
-            carbs: 98,
-            protein: 0,
-            sodium: 38,
-            calcium: '0%',
-            iron: '2%',
-          },
-          {
-            name: 'Honeycomb',
-            calories: 408,
-            fat: 3.2,
-            carbs: 87,
-            protein: 6.5,
-            sodium: 562,
-            calcium: '0%',
-            iron: '45%',
-          },
-          {
-            name: 'Donut',
-            calories: 452,
-            fat: 25.0,
-            carbs: 51,
-            protein: 4.9,
-            sodium: 326,
-            calcium: '2%',
-            iron: '22%',
-          },
-          {
-            name: 'KitKat',
-            calories: 518,
-            fat: 26.0,
-            carbs: 65,
-            protein: 7,
-            sodium: 54,
-            calcium: '12%',
-            iron: '6%',
-          },
-        ],
-      }
+import { evntBus } from '../../bus';
+import Customer from '../pos/Customer.vue'
+
+export default {
+  components: {
+    Customer
+  },
+  data () {
+    return {
+      pos_profile: '',
+      pos_opening_shift: '',
+      customer: '',
+    }
+  },
+  computed: {
+
+  },
+  created: function() {
+    this.$nextTick(function() {
+        this.check_opening_entry()
+          evntBus.$on('update_customer', (customer) => {
+          this.customer = customer;
+        });
+    })
+  },
+  methods: {
+    check_opening_entry() {
+      return frappe
+        .call('posawesome.posawesome.api.posapp.check_opening_shift', {
+          user: frappe.session.user,
+        })
+        .then((r) => {
+          if (r.message) {
+            this.pos_profile = r.message.pos_profile;
+            this.pos_opening_shift = r.message.pos_opening_shift;
+            this.get_offers(this.pos_profile.name);
+            evntBus.$emit('register_pos_profile', r.message);
+            evntBus.$emit('set_company', r.message.company);
+            console.info('LoadPosProfile');
+          } else {
+            this.create_opening_voucher();
+          }
+        });
     },
-    computed: {
-      numberOfPages () {
-        return Math.ceil(this.items.length / this.itemsPerPage)
-      },
-      filteredKeys () {
-        return this.keys.filter(key => key !== 'Name')
-      },
-    },
-    methods: {
-      nextPage () {
-        if (this.page + 1 <= this.numberOfPages) this.page += 1
-      },
-      formerPage () {
-        if (this.page - 1 >= 1) this.page -= 1
-      },
-      updateItemsPerPage (number) {
-        this.itemsPerPage = number
-      },
-    },
-  }
+  },
+}
 <\/script>`;
     if (!component.render) {
       component.render = template.render;
@@ -24371,4 +24009,4 @@ export default {
  * Based on Underscore.js 1.8.3 <http://underscorejs.org/LICENSE>
  * Copyright Jeremy Ashkenas, DocumentCloud and Investigative Reporters & Editors
  */
-//# sourceMappingURL=posapp.min.bundle.JTTCTMTP.js.map
+//# sourceMappingURL=posapp.min.bundle.BWSOOYUK.js.map
